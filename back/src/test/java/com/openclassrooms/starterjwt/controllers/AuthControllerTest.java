21b03394870c499f7311d11b7b88dfd9d7dac840
gdaume24 <geoffroy.daumer@outlook.fr>
2025-01-30 07:11:50 +0100

    maj

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
@@ -1,12 +1,24 @@
 package com.openclassrooms.starterjwt.controllers;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.starterjwt.payload.request.LoginRequest;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
@@ -21,22 +33,30 @@ public class AuthControllerTest {
     private JwtUtils jwtUtils;
     @Mock
     UserRepository userRepository;
-
+    private MockMvc mockMvc;
     private AuthController authController;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        authController = new AuthController(authenticationManager, passwordEncoder, jwtUtils, userRepository);
+        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
     }
+        
 
     @Test
     public void testAuthenticateUser() {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setEmail("test@example.com");
         loginRequest.setPassword("password");
-
+        MockHttpServletRequest request = new MockHttpServletRequest();
+            request.setParameter("loginRequest", new ObjectMapper().writeValueAsString(loginRequest));
+            
+        Authentication authentication = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content();
     }
     
 }
